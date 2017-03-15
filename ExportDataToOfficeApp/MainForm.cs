bd8d3240b5700bef3121f175a39fc6e72631df20
Sergey Lobachev <ImmersiveS@users.noreply.github.com>
2017-03-15 20:54:12 +0200

    Implement the export to Excel method

--- ExportDataToOfficeApp/MainForm.cs
+++ ExportDataToOfficeApp/MainForm.cs
@@ -50,5 +50,46 @@ namespace ExportDataToOfficeApp
             }
             UpdateGrid();
         }
+
+        private void btnExportExcel_Click(object sender, EventArgs e)
+        {
+            ExportToExcel(carsInStock);
+        }
+
+        static void ExportToExcel(List<Car> carsInStock)
+        {
+            //Load up excel and then make a new empty workbook
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Visible = true;
+            excelApp.Workbooks.Add();
+
+            //It uses single worksheet
+            Excel._Worksheet workSheet = excelApp.ActiveSheet;
+
+            //Establish column headings in cells
+            workSheet.Cells[1, "A"] = "Color";
+            workSheet.Cells[1, "B"] = "Make";
+            workSheet.Cells[1, "C"] = "PetName";
+
+            //Map all data in List<Car> to the cells of the spreadsheet
+            int row = 1;
+            foreach (var c in carsInStock)
+            {
+                row++;
+                workSheet.Cells[row, "A"] = c.Color;
+                workSheet.Cells[row, "B"] = c.Make;
+                workSheet.Cells[row, "C"] = c.PetName;
+            }
+
+            //Customize table data style
+            workSheet.Range["A1"].AutoFormat(
+                Excel.XlRangeAutoFormat.xlRangeAutoFormatClassic2);
+
+            //Save the file, quit Excel and display message to user
+            workSheet.SaveAs(string.Format(@"{0}\Inventory.xslx", Environment.CurrentDirectory));
+            excelApp.Quit();
+            MessageBox.Show("The Inventory.xlsx file has been saved to your app folder",
+                "Export complete!");
+        }
     }
 }
